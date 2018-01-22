a043f87ae6414ca09c4a55d300306749c814d7c4
yvesandre-middleton <yvesmiddleton@gmail.com>
2018-01-22 21:17:06 +0000

    fixed file path to actually download image

--- the-request.js
+++ the-request.js
@@ -1,13 +1,16 @@
 var request = require('request');
 var fs = require('fs');
 
-  request.get('https://sytantris.github.io/http-examples')
+  request.get('https://sytantris.github.io/http-examples/future.jpg')
   .on('error', function (err) {
          throw err;
   })
     .on('response', function (response) {
-         console.log(response.statusMessage, response.headers['content-type']);
-         console.log('Downloading image...');
+      console.log(response.statusMessage, response.headers['content-type']);
+      console.log('Downloading image...');
+      response.on('end', function() {
+        console.log('Download complete.');
+      })
     })
     .pipe(fs.createWriteStream('./future.jpg'));
-    console.log('Download complete.');
+
